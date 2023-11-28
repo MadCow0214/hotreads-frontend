3179bd6d943f495a0bfed1060bd60cadce27ffaf
sonhansung <sonhansung@gmail.com>
2023-11-28 22:23:32 +0900

    Update Client.js

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: process.env.REACT_APP_BACKEND_URL,
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
   clientState: {
     defaults,
     resolvers
